9e452dc6ab1861d654daf5fa8d331154face29da
alex hawley <alex@engage2learn.org>
2021-03-02 02:12:14 -0600

    - Still first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'keep-my-score';
+
+  yourScore = 0;
+  theirScore = 0;
+
+  increaseYourScore() {
+    this.yourScore += 1;
+  }
+
+  decreaseYourScore() {
+    this.yourScore -= 1;
+  }
+
+  increaseTheirScore() {
+    this.theirScore += 1;
+  }
+
+  decreaseTheirScore() {
+    this.theirScore -= 1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';;
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NzLayoutModule} from 'ng-zorro-antd/layout';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -10,9 +22,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzLayoutModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
